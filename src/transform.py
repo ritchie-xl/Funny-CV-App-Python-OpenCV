adc46e3a8501f279bbe892f6db9064a31360687c
Lei Xia <ritchie_xl@hotmail.com>
2015-06-18 23:40:53 -0400

    Fix rectangle bottom left coordinate

--- src/transform.py
+++ src/transform.py
@@ -10,7 +10,7 @@ def order_points(pts):
 
     diff = np.diff(pts,axis=1)
     rect[1] = pts[np.argmin(diff)]
-    rect[3] = pts[np.argmin(diff)]
+    rect[3] = pts[np.argmax(diff)]
 
     return rect
 
@@ -18,12 +18,12 @@ def four_point_transform(image,pts):
     rect = order_points(pts)
     (tl,tr,br,bl) = rect
 
-    widthA = np.sqrt(((br[0]-bl[0])**2)+(br[1]-bl[1])**2)
-    widthB = np.sqrt(((tr[0]-tl[0])**2)+(tr[1]-tl[1])**2)
+    widthA = np.sqrt(((br[0]-bl[0]) ** 2)+((br[1]-bl[1]) ** 2))
+    widthB = np.sqrt(((tr[0]-tl[0]) ** 2)+((tr[1]-tl[1]) ** 2))
     maxWidth = max(int(widthA),int(widthB))
 
-    heightA = np.sqrt((tr[0]-br[0])**2 + (tr[1]-br[1])**2)
-    heightB = np.sqrt((tl[0]-bl[0])**2 + (tl[1]-bl[1])**2)
+    heightA = np.sqrt(((tr[0]-br[0]) ** 2) + ((tr[1]-br[1]) ** 2))
+    heightB = np.sqrt(((tl[0]-bl[0]) ** 2) + ((tl[1]-bl[1]) ** 2))
     maxHeight = max(int(heightA),int(heightB))
 
     dst = np.array([
@@ -33,6 +33,6 @@ def four_point_transform(image,pts):
         [0,maxHeight-1]],dtype="float32")
 
     M = cv2.getPerspectiveTransform(rect,dst)
-    warped = cv2.warpPerspective(image, M, (maxWidth,maxHeight))
+    warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
 
     return warped
